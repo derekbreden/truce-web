452efb3403404471aedd37b72ed8397e1e4614b3
Derek Bredensteiner <derekbreden@gmail.com>
2024-07-29 09:01:35 +0000

    Privacy policy

--- client/loadingPage.js
+++ client/loadingPage.js
@@ -48,13 +48,16 @@ const loadingPage = (first_render, skip_state, clicked_back) => {
   // Render Share Button on topic
   renderShare();
 
+  // Welcome page
   if (state.path === "/") {
     $("main-content-wrapper[active] main-content").replaceChildren(
       $(
         `
         topics
           topic
-            h2 Welcome
+            h2[welcome]
+              span Welcome
+              $1
             p If you agree to:
             ul
               li Never escalate
@@ -64,13 +67,17 @@ const loadingPage = (first_render, skip_state, clicked_back) => {
             p
               a[big][href=/topics] Join the Discussion
         `,
+        [ $("icons icon[welcome] svg").cloneNode(true) ]
       ),
     );
     $("main-content-wrapper[active] main-content-2").replaceChildren(
       $(
         `
         topics
-            h2 Our Approach to Moderation
+          topic
+            h2[moderation]
+              span Our Approach to Moderation
+              $1
             p If you post:
             ul
               li An escalation
@@ -85,7 +92,7 @@ const loadingPage = (first_render, skip_state, clicked_back) => {
               li Nothing is blocked
             comments
               expand-wrapper[above-comments]
-                p Example
+                p Examples
               comment
                 h3 John Doe:
                 p You are a fascist, who attended a fascist rally and supported a fascist leader.
@@ -93,11 +100,27 @@ const loadingPage = (first_render, skip_state, clicked_back) => {
                   info
                     b Name-calling
                     p If someone does not identify themselves as a fascist, calling them one is an example of name-calling. This type of labeling hinders constructive and respectful dialogue.
+              comment
+                h3 Jane Doe:
+                p Sometimes violence is the answer.
+                  info
+                    b Escalation
+                    p This statement suggests that violence can be a solution, which promotes conflict and hostility rather than peaceful dialogue.
+              comment
+                h3 Sam Smith:
+                p They are pure evil.
+                  info
+                    b Judgement
+                    p Labeling anyone as "pure evil" is a critical judgment that hinders respectful dialogue and constructive conversation.
+        topics
+          p[style="opacity:.5;text-align:center;"]
+            a[href="/privacy"] Privacy Policy
         `,
+        [ $("icons icon[communication] svg").cloneNode(true) ]
       ),
     );
 
-    $("main-content-wrapper[active] main-content")
+    $("main-content-wrapper[active")
       .$("[href]")
       .forEach(($el) => {
         $el.on("click", ($event) => {
@@ -105,5 +128,31 @@ const loadingPage = (first_render, skip_state, clicked_back) => {
           goToPath($el.getAttribute("href"));
         });
       });
+  }
+
+  // Privacy page
+  if (state.path === "/privacy") {
+    $("main-content-wrapper[active] main-content").replaceChildren(
+      $(
+        `
+        topics
+          topic
+            h2 Privacy Policy
+            p[bold] Information Collection
+            p We do not collect any personal data from users of our app. The email address and password you provide are used solely to facilitate account recovery.
+            p[bold] Use of Information
+            p Your email and password are used solely to facilitate account recovery.
+            p[bold] Data Security
+            p We take measures to protect your information from unauthorized access or disclosure.
+            p[bold] No Third-Party Services
+            p We do not use any third-party services or analytics.
+            p[bold] Policy Changes
+            p We may update this policy from time to time. Changes will be effective immediately upon posting.
+            p[bold] Contact
+            p For any questions, contact us at derek@truce.net
+        `,
+        [ $("icons icon[welcome] svg").cloneNode(true) ]
+      ),
+    );
   }
 };
