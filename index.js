b124e64a1ab0cec50a4fdf793b258c0ecfd23a17
Derek Bredensteiner <derekbreden@gmail.com>
2025-06-05 23:37:31 -0500

    lint fix

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const pool = require("./server/pool")
 const email = require("./server/email")
 const ai = require("./server/ai")
 const schema = require("./server/schema")
-const server = require("./server/server")(
-	// Init
-	async () => {
-		pool.init()
-		email.init()
-		ai.init()
-		await schema.init()
-		await server.init()
-	},
-)()
+const server = require("./server/server")
+
+// Init
+;(async () => {
+	pool.init()
+	email.init()
+	ai.init()
+	await schema.init()
+	await server.init()
+})()
