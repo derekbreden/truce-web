0055bf564374efc144a2c37419d02f9bc2357bad
Derek Bredensteiner <derekbreden@gmail.com>
2025-06-10 23:29:02 -0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,31 @@
 export default [
-    {
-        languageOptions: {
-            ecmaVersion: 2021,
-            sourceType: "module",
-            globals: {
-                console: "readonly",
-                window: "readonly"
-            }
-        },
-        rules: {
-            "semi": ["error", "never"],
-            "quotes": ["error", "double"],
-            "eqeqeq": ["error", "always"],
-            "prefer-arrow-callback": "error",
-            "func-style": ["error", "expression"],
-            "max-len": ["error", { "code": 120 }],
-            "operator-linebreak": ["error", "before", {
-                "overrides": {
-                    "=": "after",
-                    "+=": "after",
-                    "-=": "after",
-                    "*=": "after",
-                    "/=": "after"
-                }
-            }],
-            "allowTemplateLiterals": true,
-        }
-    }
+	{
+		languageOptions: {
+			ecmaVersion: 2021,
+			sourceType: "module",
+			globals: {
+				console: "readonly",
+				window: "readonly"
+			}
+		},
+		rules: {
+			"semi": ["error", "never"],
+			"quotes": ["error", "double", {
+				"allowTemplateLiterals": true
+			}],
+			"eqeqeq": ["error", "always"],
+			"prefer-arrow-callback": "error",
+			"func-style": ["error", "expression"],
+			"max-len": ["error", { "code": 120 }],
+			"operator-linebreak": ["error", "before", {
+				"overrides": {
+					"=": "after",
+					"+=": "after",
+					"-=": "after",
+					"*=": "after",
+					"/=": "after"
+				}
+			}],
+		}
+	}
 ]
