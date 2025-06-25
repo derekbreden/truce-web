41241cfc32afd39f118e544f6c7473c16eb4d67b
Derek Bredensteiner <derekbreden@gmail.com>
2025-06-25 02:00:49 -0500

    Make postgres use Number() instead of String()

--- server/pool.js
+++ server/pool.js
@@ -1,8 +1,15 @@
-const { Pool } = require("pg")
+const { Pool, types } = require("pg")
 module.exports = {
 	init() {
 		this.pool = new Pool({
 			connectionString: process.env.DATABASE_URL,
+		})
+
+		// Override type parser to use Number instead of string (we don't care about big numbers)
+		;[20, 21, 23, 700, 701, 1700].forEach((typeId) => {
+			types.setTypeParser(typeId, (val) => {
+				return Number(val)
+			})
 		})
 	},
 }
